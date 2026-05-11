b4e4e414534348000a944565d077d2a8170d206b
allan0 <allan.mrthi@gmail.com>
2026-05-11 14:36:12 +0400

    feat: build and deploy fixed, app colors and loader synced

---
+++ android/app/src/main/kotlin/com/example/cabal/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cabal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   // Core Branded Colors
   static const Color background = Color(0xFF0F0F0F);
-  static const Color offBlack = Color(0xFF080808); // Added this
+  static const Color offBlack = Color(0xFF080808); 
   static const Color gold = Color(0xFFD4AF37);
   static const Color primaryAccent = Color(0xFFE2B05E);
   static const Color darkGrey = Color(0xFF1B1212);
@@ -13,7 +13,7 @@ class AppColors {
   // Status Colors
   static const Color success = Color(0xFF4CAF50);
   static const Color error = Color(0xFFF44336);
-  static const Color info = Color(0xFF2196F3);
+  static const Color info = Color(0xFF2196F3); // Added this to fix the crash
 
   // Gradients
   static const Gradient cardOverlayGradient = LinearGradient(
@@ -23,5 +23,5 @@ class AppColors {
       Color(0x22FFFFFF),
       Color(0x00FFFFFF),
     ],
-  ); // Added this
+  );
 }
